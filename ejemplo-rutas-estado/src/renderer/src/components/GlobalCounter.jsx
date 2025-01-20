d0b6a1b396f9ae9e935cd825ee4ed803699b750b
Javier Garcia <javgarmun3@alu.edu.gva.es>
2025-01-20 15:42:53 +0100

    rehacer GlobalCounter

--- ejemplo-rutas-estado/src/renderer/src/components/GlobalCounter.jsx
+++ ejemplo-rutas-estado/src/renderer/src/components/GlobalCounter.jsx
@@ -2,19 +2,16 @@ import { useSelector, useDispatch } from 'react-redux'
 import { increment, decrement } from '../redux/counter-slice'
 
 export function GlobalCounter() {
-  // Obtiene el valor actual del contador desde el estado global de Redux
   const counter = useSelector((state) => state.counter.value)
-
-  // Obtiene la función dispatch para enviar acciones al store de Redux
   const dispatch = useDispatch()
 
   return (
     <div>
       <h2>Counter: {counter}</h2>
-      {/* Botón para incrementar el contador */}
-      <button onClick={() => dispatch(increment())}>Increment</button>
-      {/* Botón para decrementar el contador */}
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      {/* Uso de funciones vacías para que los dispatch no se ejecuten automáticamente al renderizar los botones.
+      Por tanto, solo funcionarán al hacer clic sobre ellos */}
+      <button onClick={() => dispatch(increment())}>Increment counter</button>
+      <button onClick={() => dispatch(decrement())}>Decrement counter</button>
     </div>
   )
 }
